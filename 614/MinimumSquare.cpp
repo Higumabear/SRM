607b109a52a5a6765fb077da94089845da327968
Higumabear <slow.thursday.night@gmail.com>
2014-04-09 04:53:54 +0900

    614 Div1 Easy

---
+++ 614/MinimumSquare.cpp
@@ -0,0 +1,110 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <climits>
+#include <cfloat>
+#include <map>
+#include <utility>
+#include <set>
+#include <iostream>
+#include <memory>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <functional>
+#include <sstream>
+#include <complex>
+#include <stack>
+#include <queue>
+using namespace std;
+static const double EPS = 1e-8;
+static const int INF= 1 << 29;
+#define ALL(c) (c).begin(), (c).end()
+typedef long long ll;
+
+struct p{
+  ll x, y;
+  bool operator<(const p &r)const{
+    if(x != r.x) return x < r.x;
+    else return y <= r.y;
+  }
+};
+
+struct q{
+  ll x, y;
+  bool operator<(const q &r)const{
+    if(y != r.y) return y < r.y;
+    else return x <= r.x;
+  }
+};
+
+
+class MinimumSquare {
+public:
+
+  ll labs(ll t){
+    return t > 0 ? t : -t;
+  }
+  long long minArea(vector <int> x, vector <int> y, int K) {
+    long long result  = 1LL << 62;
+    int N = x.size();
+    
+    for(int i = 0; i < N; i++){
+      for(int j = 0; j < N; j++){
+	if(x[j] < x[i]) continue;
+	
+	for(int k = 0; k < N; k++){
+	  ll w = x[j] - x[i];
+
+	  int cnt = 0;
+	  for(int l = 0; l < N; l++)
+	    if(x[i] <= x[l] && x[l] <= x[j] &&
+	       y[k] <= y[l] && y[l] <= y[k] + w) cnt++;
+	  
+	  if(cnt >= K) result = min(result, (w + 2) * (w + 2));
+	}
+      }
+    }
+    for(int i = 0; i < N; i++){
+      for(int j = 0; j < N; j++){
+	if(y[j] < y[i]) continue;
+	
+	for(int k = 0; k < N; k++){
+	  ll h = y[j] - y[i];
+
+	  int cnt = 0;
+	  for(int l = 0; l < N; l++)
+	    if(y[i] <= y[l] && y[l] <= y[j] &&
+	       x[k] <= x[l] && x[l] <= x[k] + h) cnt++;
+	  
+	  if(cnt >= K) result = min(result, (h + 2) * (h + 2));
+	}
+      }
+    }
+
+
+    return result;
+  }
+  
+// BEGIN CUT HERE
+	public:
+	void run_test(int Case) { if ((Case == -1) || (Case == 0)) test_case_0(); if ((Case == -1) || (Case == 1)) test_case_1(); if ((Case == -1) || (Case == 2)) test_case_2(); if ((Case == -1) || (Case == 3)) test_case_3(); if ((Case == -1) || (Case == 4)) test_case_4(); }
+	private:
+	template <typename T> string print_array(const vector<T> &V) { ostringstream os; os << "{ "; for (typename vector<T>::const_iterator iter = V.begin(); iter != V.end(); ++iter) os << '\"' << *iter << "\","; os << " }"; return os.str(); }
+	void verify_case(int Case, const long long &Expected, const long long &Received) { cerr << "Test Case #" << Case << "..."; if (Expected == Received) cerr << "PASSED" << endl; else { cerr << "FAILED" << endl; cerr << "\tExpected: \"" << Expected << '\"' << endl; cerr << "\tReceived: \"" << Received << '\"' << endl; } }
+	void test_case_0() { int Arr0[] = {0, 3}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arr1[] = {0, 7}; vector <int> Arg1(Arr1, Arr1 + (sizeof(Arr1) / sizeof(Arr1[0]))); int Arg2 = 2; long long Arg3 = 81LL; verify_case(0, Arg3, minArea(Arg0, Arg1, Arg2)); }
+	void test_case_1() { int Arr0[] = {-4, 3, 1}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arr1[] = {3 , -1, -2}; vector <int> Arg1(Arr1, Arr1 + (sizeof(Arr1) / sizeof(Arr1[0]))); int Arg2 = 2; long long Arg3 = 16LL; verify_case(1, Arg3, minArea(Arg0, Arg1, Arg2)); }
+	void test_case_2() { int Arr0[] = {0, 0, 1, 1, 2, 2}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arr1[] = {0, 1, 0, 1, 0, 1}; vector <int> Arg1(Arr1, Arr1 + (sizeof(Arr1) / sizeof(Arr1[0]))); int Arg2 = 4; long long Arg3 = 9LL; verify_case(2, Arg3, minArea(Arg0, Arg1, Arg2)); }
+	void test_case_3() { int Arr0[] = {1000000000, 1000000000, 1000000000, -1000000000, -1000000000, -1000000000}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arr1[] = {1000000000, 0, -1000000000, 1000000000, 0, -1000000000}; vector <int> Arg1(Arr1, Arr1 + (sizeof(Arr1) / sizeof(Arr1[0]))); int Arg2 = 3; long long Arg3 = 4000000008000000004LL; verify_case(3, Arg3, minArea(Arg0, Arg1, Arg2)); }
+	void test_case_4() { int Arr0[] = {-47881, 28623, 1769, -38328, -16737, 16653, -23181, 37360, 41429, 26282, 254, 728, 8299, -41080, -29498, 17488, -23937, -11, 33319, 25232}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arr1[] = {-46462, 48985, -43820, -19587, -33593, -28337, 13667, -48131, -5568, -2332, -41918, -31370, -3695, 42599, -37788, -40096, 39049, 25045, -2122, 3874}; vector <int> Arg1(Arr1, Arr1 + (sizeof(Arr1) / sizeof(Arr1[0]))); int Arg2 = 8; long long Arg3 = 1695545329LL; verify_case(4, Arg3, minArea(Arg0, Arg1, Arg2)); }
+
+// END CUT HERE
+
+};
+
+// BEGIN CUT HERE 
+int main() {
+  MinimumSquare ___test;
+  ___test.run_test(-1);
+}
+// END CUT HERE
