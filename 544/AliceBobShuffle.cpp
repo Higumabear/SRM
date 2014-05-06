8f2da3419c9efdf03e6a6d3ec64f976c7063fb58
Higumabear <slow.thursday.night@gmail.com>
2014-05-06 09:50:49 +0900

    544 Div2 Hard

---
+++ 544/AliceBobShuffle.cpp
@@ -0,0 +1,78 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <climits>
+#include <cfloat>
+#include <map>
+#include <utility>
+#include <set>
+#include <iostream>
+#include <memory>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <functional>
+#include <sstream>
+#include <complex>
+#include <stack>
+#include <queue>
+#include <cstring>
+using namespace std;
+static const double EPS = 1e-8;
+static const int INF= 1 << 29;
+#define ALL(c) (c).begin(), (c).end()
+typedef long long ll;
+
+int memo[51][51][51][51];
+const int MOD = 1000000007;
+
+class AliceBobShuffle {
+public:
+  vector<int> as, bs, ae, be;
+  ll rec(int a, int b, int c, int d){
+    if(a == as.size() && b == bs.size() && 
+       c == ae.size() && d == be.size()) return 1;
+    if(memo[a][b][c][d] >= 0) return memo[a][b][c][d];
+    
+    ll ret = 0;
+    if(a < (int)as.size() && c < (int)ae.size() && as[a] == ae[c]) {
+     ret = (ret + rec(a + 1, b, c + 1, d)) % MOD;   
+    }
+    if(a < (int)as.size() && d < (int)be.size() && as[a] == be[d]) 
+      ret = (ret + rec(a + 1, b, c, d + 1)) % MOD;
+    if(b < (int)bs.size() && c < (int)ae.size() && bs[b] == ae[c]) 
+      ret = (ret + rec(a, b + 1, c + 1, d)) % MOD;
+    if(b < (int)bs.size() && d < (int)be.size() && bs[b] == be[d]) 
+      ret = (ret + rec(a, b + 1, c, d + 1)) % MOD;
+
+    //cout << "koko" << ret << " ";
+    return memo[a][b][c][d] = ret;;
+  }
+  int countWays(vector <int> AliceStart, vector <int> BobStart, vector <int> AliceEnd, vector <int> BobEnd) {
+    memset(memo, -1, sizeof(memo));
+    as = AliceStart; bs = BobStart; ae = AliceEnd; be = BobEnd;
+    return rec(0, 0, 0, 0) % MOD;
+  }
+  
+// BEGIN CUT HERE
+	public:
+	void run_test(int Case) { if ((Case == -1) || (Case == 0)) test_case_0(); if ((Case == -1) || (Case == 1)) test_case_1(); if ((Case == -1) || (Case == 2)) test_case_2(); if ((Case == -1) || (Case == 3)) test_case_3(); if ((Case == -1) || (Case == 4)) test_case_4(); }
+	private:
+	template <typename T> string print_array(const vector<T> &V) { ostringstream os; os << "{ "; for (typename vector<T>::const_iterator iter = V.begin(); iter != V.end(); ++iter) os << '\"' << *iter << "\","; os << " }"; return os.str(); }
+	void verify_case(int Case, const int &Expected, const int &Received) { cerr << "Test Case #" << Case << "..."; if (Expected == Received) cerr << "PASSED" << endl; else { cerr << "FAILED" << endl; cerr << "\tExpected: \"" << Expected << '\"' << endl; cerr << "\tReceived: \"" << Received << '\"' << endl; } }
+	void test_case_0() { int Arr0[] = {1, 3}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arr1[] = {2, 4}; vector <int> Arg1(Arr1, Arr1 + (sizeof(Arr1) / sizeof(Arr1[0]))); int Arr2[] = {2, 3}; vector <int> Arg2(Arr2, Arr2 + (sizeof(Arr2) / sizeof(Arr2[0]))); int Arr3[] = {1, 4}; vector <int> Arg3(Arr3, Arr3 + (sizeof(Arr3) / sizeof(Arr3[0]))); int Arg4 = 4; verify_case(0, Arg4, countWays(Arg0, Arg1, Arg2, Arg3)); }
+	void test_case_1() { int Arr0[] = {1, 2, 1}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arr1[] = {1, 2}; vector <int> Arg1(Arr1, Arr1 + (sizeof(Arr1) / sizeof(Arr1[0]))); int Arr2[] = {1, 2, 1}; vector <int> Arg2(Arr2, Arr2 + (sizeof(Arr2) / sizeof(Arr2[0]))); int Arr3[] = {2, 1}; vector <int> Arg3(Arr3, Arr3 + (sizeof(Arr3) / sizeof(Arr3[0]))); int Arg4 = 4; verify_case(1, Arg4, countWays(Arg0, Arg1, Arg2, Arg3)); }
+	void test_case_2() { int Arr0[] = {1}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arr1[] = {2}; vector <int> Arg1(Arr1, Arr1 + (sizeof(Arr1) / sizeof(Arr1[0]))); int Arr2[] = {3}; vector <int> Arg2(Arr2, Arr2 + (sizeof(Arr2) / sizeof(Arr2[0]))); int Arr3[] = {4}; vector <int> Arg3(Arr3, Arr3 + (sizeof(Arr3) / sizeof(Arr3[0]))); int Arg4 = 0; verify_case(2, Arg4, countWays(Arg0, Arg1, Arg2, Arg3)); }
+	void test_case_3() { int Arr0[] = {1, 2}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arr1[] = {3}; vector <int> Arg1(Arr1, Arr1 + (sizeof(Arr1) / sizeof(Arr1[0]))); int Arr2[] = {3}; vector <int> Arg2(Arr2, Arr2 + (sizeof(Arr2) / sizeof(Arr2[0]))); int Arr3[] = {2, 1}; vector <int> Arg3(Arr3, Arr3 + (sizeof(Arr3) / sizeof(Arr3[0]))); int Arg4 = 0; verify_case(3, Arg4, countWays(Arg0, Arg1, Arg2, Arg3)); }
+	void test_case_4() { int Arr0[] = {3, 3, 2, 4, 1, 3, 1, 2, 1, 1, 5, 5, 1, 2, 3, 1, 2, 1, 2, 1, 1, 1, 2}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arr1[] = {4, 1, 4, 3, 4, 4, 4, 3, 2, 1, 6, 2, 2, 3, 2, 2, 1, 3, 2, 3}; vector <int> Arg1(Arr1, Arr1 + (sizeof(Arr1) / sizeof(Arr1[0]))); int Arr2[] = {4, 1, 4, 3, 4, 4, 4, 1, 3, 1, 2, 2, 2, 3, 2, 1, 2, 1, 2, 2, 2}; vector <int> Arg2(Arr2, Arr2 + (sizeof(Arr2) / sizeof(Arr2[0]))); int Arr3[] = {3, 3, 2, 3, 4, 2, 1, 2, 1, 1, 5, 6, 5, 1, 3, 1, 2, 3, 1, 1, 1, 3}; vector <int> Arg3(Arr3, Arr3 + (sizeof(Arr3) / sizeof(Arr3[0]))); int Arg4 = 314159265; verify_case(4, Arg4, countWays(Arg0, Arg1, Arg2, Arg3)); }
+
+// END CUT HERE
+
+};
+
+// BEGIN CUT HERE 
+int main() {
+  AliceBobShuffle ___test;
+  ___test.run_test(-1);
+}
+// END CUT HERE
